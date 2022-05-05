c11da0e7b59af278cab04de7337c1d6f79f8ed10
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-05 12:24:12 -0700

    wrap App.jsx return in UserProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Switch, Route } from 'react-router-dom';
 import Auth from './views/Auth/Auth';
 import Dashboard from './views/Dashboard/Dashboard';
+import { UserProvider } from './context/UserContext';
 import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
-    <Switch >
-      <Route path='/login'>
+    <UserProvider>
+      <Switch >
+        <Route path='/login'>
         <Auth />
-      </Route>
-      <PrivateRoute path='/dashboard'>
+        </Route>
+        <PrivateRoute path='/dashboard'>
         <Dashboard />
-      </PrivateRoute>
-    </Switch>
+        </PrivateRoute>
+      </Switch>
+    </UserProvider>
   );
 }
