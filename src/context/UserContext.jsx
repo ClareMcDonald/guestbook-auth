5c7d76cdf6d1109fba5f6369571a9a71e9bcfb08
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-05 11:27:37 -0700

    finished context file

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -8,6 +8,30 @@ export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(currentUser || { email: null });
 
     const login = async (email, password) => {
+        const authenticatedUser = await signInUser({ email, password });
 
+        if (authenticatedUser) {
+            setUser(authenticatedUser)
+        }
+    };
+
+    const logout = () => {
+        setUser({ email: null });
+    };
+
+    return (
+        <UserContext.Provider value={{ user, login, logout }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+    
+export const useUser = () => {
+    const context = useContext(UserContext);
+
+    if (context === undefined) {
+        throw new Error('You must use useUser within context');
     }
+
+    return context;
 }
