2ee0a4f8ce0f82bb181fe1c0b7d26b4c8fea5267
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-06 10:29:46 -0700

    set default state, getEntries useEffect, and mapping through entries in Dashboard

---
+++ src/components/EntryForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EntryForm() {
+  return (
+    <div>EntryForm</div>
+  )
+}
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -3,7 +3,27 @@ import { useUser } from '../../context/UserContext';
 import { getEntries } from '../../services/entries';
 
 export default function Dashboard() {
+  const { logout } = useUser();
+  const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getEntries()
+      .then(setEntries)
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h2>Dashboard</h2>
+      <button onClick={logout}>Sign Out</button>
+      {loading
+        ? <p>Loading entries ^_^</p>
+        : <ul>{entries.map((entry) => (
+          <li key={entry.id}>{entry.content}</li>
+        ))}</ul>
+      }
+    </>
   )
 }
