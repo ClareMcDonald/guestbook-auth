6cc80328c0011315fac79b52098b1280ce691889
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-06 10:09:12 -0700

    wrote entries services

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useUser } from '../../context/UserContext';
+import { getEntries } from '../../services/entries';
 
 export default function Dashboard() {
   return (
